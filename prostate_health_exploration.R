4e514a1d8e5bc325e903e187b39f00c9759de9b3
Garrett Herrington <patrickgherrington@yahoo.com>
2025-07-30 15:46:19 -0500

    Exploratory Data Analysis

--- prostate_health_exploration.R
+++ prostate_health_exploration.R
@@ -1,4 +1,38 @@
 library(tidyverse)
+install.packages("ggplot2")
 
-df <- read.csv('/Users/rworkspace/Downloads/synthetic_prostate_cancer_risk.csv')
+patients_df <- read.csv('/Users/rworkspace/Downloads/synthetic_prostate_cancer_risk.csv')
+
+glimpse(patients_df)
+
+head(patients_df)
+
+patients_df$risk_level <- factor(patients_df$risk_level, 
+                                 levels = c("Low", "Medium", "High"))
+
+
+
+?factor
+
+patient_smokers <- patient_smokers$risk_level <- factor(patient_smokers$risk_level, levels = c("Low", "Medium", "High"))
+
+ggplot(data = patients_df,
+       mapping = aes(x = risk_level, fill = risk_level)
+       ) + 
+        geom_bar() + 
+        geom_text(stat = 'count', aes(label = ..count..),
+                  vjust = -0.3, size = 3) +
+        labs(
+          x = "Risk Category",
+          y = "Number of Patients",
+          fill = "Risk Level",
+          title = "Amount of Patients Per Risk Level"
+        )
+        scale_fill_manual(values = c("Low" = 'blue',
+                                     'Medium' = 'green',
+                                     'High' = 'orange')) +
+        theme_minimal()
+        
+levels(patients_df$risk_level)
+
 
